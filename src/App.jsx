3030e667bf2041dc175cdaeb3487d697ca38fc2c
Luthiwd <annattar@gmail.com>
2023-02-15 11:44:59 +0100

    custom hook useMovies and Search(this in app.jsx) grid style responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,70 @@
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import responseMovies from './mocks/with-result.json'
-import withoutResults from './mocks/no-result.json'
 import { Movies } from './components/Movies'
+import { useMovies } from './hooks/useMovies'
+
+function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
 
 function App() {
-  const movies = responseMovies.Search
+  
+  const { movies } = useMovies()
+  const { search, updateSearch, error } = useSearch()
+
+  const handleSubmit  = (event) => {
+    event.preventDefault()
+    console.log({search});
+  }
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
+  }
+
 
   return (
   
   <div className='page'>
       <header>
         <h1>Buscador de Películas</h1>
-        <form className=''>
-          <input placeholder='Avengers, Star Wars, The Matrix ...' />
+        <form className='form' onSubmit={handleSubmit}>
+        <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Wars, The Matrix...'
+          />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
       <main>
         <Movies movies={movies}/>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,13 @@
+
+
 function ListOfMovies({ movies }) {
   return(
-    <ul>
+    <ul className="movies">
       {movies.map((movie) => (
-        <li key={movie.imdbID}>
-          <h3>{movie.Title}</h3>
-          <p>{movie.Year}</p>
-          <img src={movie.Poster} alt={movie.Title} />
+        <li className="movie" key={movie.id}>
+          <h3>{movie.title}</h3>
+          <p>{movie.year}</p>
+          <img src={movie.poster} alt={movie.title} />
         </li>
       ))}
     </ul>
